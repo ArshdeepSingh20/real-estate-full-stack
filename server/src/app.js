199263fe5c0f72e080cf02121a6742db3c03aeb9
Arshdeep Singh <singharshdeep9039@gmail.com>
2024-04-20 03:04:30 +0530

    updated code

--- server/src/app.js
+++ server/src/app.js
@@ -19,11 +19,6 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 // app.use(express.static("public"))
 app.use(cookieParser());
 
-app.get('/', (req,res) => {
-  res.json('api working OK!')
-})
-
-
 //routes import
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
